2e142f592a61378a100928845f37182b191c7ff0
mina-william-23 <capeandroid50@gmail.com>
2023-06-21 15:12:29 -0700

    task 12

---
+++ sq_pstr.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+
+/**
+ * sq_pstr -  prints the string starting at the top of the stack
+ * @stack: stack
+ * @line_number: line number of interpreted opened file
+ */
+void sq_pstr(stack_t **stack, unsigned int line_number)
+{
+        stack_t *tmp = *stack;
+        (void) line_number;
+
+        while (tmp)
+        {
+                if (tmp->n <= 0 || tmp->n > 127)
+                        break;
+                printf("%c", (*stack)->n);
+                tmp = tmp->next;
+        }
+        printf("\n");
+}
