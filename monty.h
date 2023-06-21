b4a7b2d25b1af91110daa1e02de2e111ac2ee1b8
mina-william-23 <capeandroid50@gmail.com>
2023-06-21 14:28:09 -0700

    add task 6

--- monty.h
+++ monty.h
@@ -71,6 +71,7 @@ void sq_pop(stack_t **stack, unsigned int line_number);
 void sq_swap(stack_t **stack, unsigned int line_number);
 void sq_add(stack_t **stack, unsigned int line_number);
 void sq_nop(stack_t **stack, unsigned int line_number);
+void sq_sub(stack_t **stack, unsigned int line_number);
 
 
 #endif
