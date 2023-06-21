4d7491b0af7346f1387fdd1e58cc9ff2a67cafa5
mina-william-23 <capeandroid50@gmail.com>
2023-06-21 15:08:39 -0700

    task 12

--- sq_pchar.c
+++ sq_pchar.c
@@ -20,3 +20,22 @@ exit(EXIT_FAILURE);
 }
 printf("%c\n", (*stack)->n);
 }
+
+/**
+ * sq_pstr -  prints the string starting at the top of the stack
+ * @stack: stack
+ * @line_number: line number of interpreted opened file
+ */
+void sq_pchar(stack_t **stack, unsigned int line_number)
+{
+        stack_t *tmp = *stack;
+
+        while (tmp)
+        {
+                if (tmp->n <= 0 || tmp->n > 127)
+                        break;
+                printf("%c", (*stack)->n);
+                tmp = tmp->next;
+        }
+        printf("\n");
+}
