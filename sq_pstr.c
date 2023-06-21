bf714939d9ba618286a6dd99499afd92977d5afc
mina-william-23 <capeandroid50@gmail.com>
2023-06-21 16:49:54 -0700

    betty fix

--- sq_pstr.c
+++ sq_pstr.c
@@ -7,15 +7,15 @@
  */
 void sq_pstr(stack_t **stack, unsigned int line_number)
 {
-        stack_t *tmp = *stack;
-        (void) line_number;
+	stack_t *tmp = *stack;
+	(void) line_number;
 
-        while (tmp)
-        {
-                if (tmp->n <= 0 || tmp->n > 127)
-                        break;
-                printf("%c", tmp->n);
-                tmp = tmp->next;
-        }
-        printf("\n");
+	while (tmp)
+	{
+		if (tmp->n <= 0 || tmp->n > 127)
+			break;
+		printf("%c", tmp->n);
+		tmp = tmp->next;
+	}
+	printf("\n");
 }
