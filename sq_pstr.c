b4e3e9a868223e87d15cfcfc0bcdc2f965e3493d
mina-william-23 <capeandroid50@gmail.com>
2023-06-21 15:17:29 -0700

    task 12

--- sq_pstr.c
+++ sq_pstr.c
@@ -14,7 +14,7 @@ void sq_pstr(stack_t **stack, unsigned int line_number)
         {
                 if (tmp->n <= 0 || tmp->n > 127)
                         break;
-                printf("%c", (*stack)->n);
+                printf("%c", tmp->n);
                 tmp = tmp->next;
         }
         printf("\n");
